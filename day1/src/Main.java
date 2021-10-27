cfcd53bd85dc3363b4ce40c725726d2459b619c1
Huseyin Kesmez <29351841+huseyinkesmez@users.noreply.github.com>
2021-10-27 14:50:20 +0300

    Add files via upload

---
+++ day1/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // ORTALAMA HESAPLAMA
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.print("mat: ");
+        double mat = input.nextInt();
+        System.out.print("fen");
+        double fen = input.nextInt();
+        System.out.print("geo");
+        double geo = input.nextInt();
+        double total = (mat + geo + fen) / 3;
+        System.out.println("derslerin ort: " + total);
+
+        if (total >=60 ) {
+            System.out.println("sinifi gecti");
+        }
+        else {
+            System.out.println("sinifta kaldi");
+        } */
+
+        // KDV HESAPLAMA
+
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.print("price: ");
+        double price = input.nextDouble();
+        if (price <=1000 && price >0) {
+            double result = price + (price*0.18);
+            double taxAmount = result - price;
+            System.out.println("tax with price: "+result);
+            System.out.printf("tax amount: " + taxAmount );
+        } if (price >1000) {
+            double result = price + (price*0.08);
+            double taxAmount = result - price;
+            System.out.println("tax with price: "+result);
+            System.out.printf("tax amount:" + taxAmount );
+        } if (price <=0) {
+            System.out.printf("ENTER VALUE BIGGER THAN ZERO !!!!");
+        }
+        */
+        // TAKSIMETRE
+       /* Scanner input = new Scanner(System.in);
+        System.out.print("KM: ");
+        int km = input.nextInt();
+        double price = (km * 2.20) + 10;
+        if (price < 20) {
+            System.out.println("Taksimetre ucreti: 20 TL");
+        } else {
+            System.out.println("Taksimetre ucreti: " + price + " TL");
+        }
+
+
+        System.out.println();*/
+
+
+    }
+}
